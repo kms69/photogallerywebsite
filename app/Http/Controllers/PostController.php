b2239208b1071f23de904b9440423c7d3abf620b
kms69 <kavehmohammadi139@gmail.com>
2021-10-06 13:33:26 +0330

    index and create added to the postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::latest()->paginate(10);
+
+        return view('posts.index',compact('posts'))
+            ->with('i', (request()->input('page', 1) - 1) * 10);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
 }
