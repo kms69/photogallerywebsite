5922cba3069630a775e699c74b840570d71850fb
kms69 <kavehmohammadi139@gmail.com>
2021-10-24 12:22:07 +0330

    Ablumcontrollerr modified

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace App\Http\Controllers\API\v1;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+
 use App\Models\Album;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class AlbumController extends Controller
 {
 
     public function index()
     {
-        $album = Album::get();
-        return view('album.index', compact('album'));
+        $albums = Album::all();
+        return view('admin.album.index', compact('albums'));
     }
     public function get($id)
     {
@@ -20,40 +21,94 @@ class AlbumController extends Controller
         return view('album',compact('album'));
 
     }
+    public function edit($id)
+    {
+
+        $album = Album::findOrFail($id);
+
+        return view('admin.album.edit', compact(['album']));
+    }
     public function create()
     {
-        return view('createalbum');
+        return view('admin.album.create');
     }
-    public function upload(Request $request)
+
+
+    public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|string|max:50',
-            'cover_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'description' => 'required|string|max:225',
+            'title' => 'required|string|max:50',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'body' => 'required|string|max:225',
 
 
         ]);
 
-        $input = $request->all();
+        $name = $request->file('image')->getClientOriginalName();
 
-        if ($image = $request->file('cover_image')) {
-            $destinationPath = 'album_image/';
-            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
-            $image->move($destinationPath, $profileImage);
-            $input['cover_image'] = "$profileImage";
+        $path = $request->file('image')->storeAs('public/album', $name);
+
+
+       $album= new Album();
+        $album->title = $request->input('title');
+        $album->body = $request->input('body');
+
+
+        $album->image = $name;
+
+        $album->save();
+        return redirect()->route('album.index')
+            ->with('success', 'album created successfully.');
+    }
+    public function update(Request $request, Album $album)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:50',
+            'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'body' => 'required|string|max:225',
+
+
+        ]);
+
+        if ($request->file('image') != '') {
+            $path = public_path("\storage\album\\") . $album->image;
+
+
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+
+
+            $name = $request->file('image')->getClientOriginalName();
+            $path = $request->file('image')->storeAs('public/album', $name);
+        } else {
+            $name = $album->image;
         }
-        Album::create($input);
+
+        $album->image = $name;
+        $album->title = $request->input('title');
+        $album->body = $request->input('body');
 
 
-        return back()
-            ->with('success', 'Image Uploaded successfully.');
+
+
+
+        $album->update();
+        return redirect()->route('album.index')
+            ->with('success', 'album created successfully.');
     }
 
 
-    public function destroy(Album $album)
+    public function destroy($id)
     {
-        $album->delete();
-        return back()
-            ->with('success', 'Image removed successfully.');
+        $album = Album::findOrFail($id);
+
+        $album->delete($id);
+        $path = public_path("\storage\album\\") . $album->image;
+        File::delete($path);
+
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Posts deleted successfully');
     }
 }
