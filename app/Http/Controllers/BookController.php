3a4919104caa46d0571de5c5a4eede243dd5d6bf
kms69 <kavehmohammadi139@gmail.com>
2021-10-24 12:23:03 +0330

    Bookcontrollerr modified

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,56 +1,118 @@
 <?php
 
-namespace App\Http\Controllers\API\v1;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class BookController extends Controller
 {
     public function index()
     {
-        $books = Book::latest()->paginate(10);
+        $books = Book::all();
+        return view('admin.book.index', compact('books'));
+    }
 
-        return view('books.index',compact('books'))
-            ->with('i', (request()->input('page', 1) - 1) * 10);
+    public function edit($id)
+    {
 
+        $book = Book::findOrFail($id);
 
+        return view('admin.book.edit', compact(['book']));
+    }
+    public function create()
+    {
+        return view('admin.book.create');
     }
 
 
-    public function create(Request $request)
+    public function store(Request $request)
     {
         $this->validate($request, [
             'title' => 'required|string|max:50',
-            'book_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'year' => 'required|bigInteger',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'year' => 'required|integer',
             'publisher' => 'required|string|max:50',
-            'body' => 'required|text',
+            'body' => 'required|string',
 
 
         ]);
 
-        $input = $request->all();
+        $name = $request->file('image')->getClientOriginalName();
 
-        if ($image = $request->file('book_image')) {
-            $destinationPath = 'exhibitions_image/';
-            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
-            $image->move($destinationPath, $profileImage);
-            $input['book_image'] = "$profileImage";
+        $path = $request->file('image')->storeAs('public/book', $name);
+
+
+        $book= new Book();
+        $book->title = $request->input('title');
+        $book->image = $name;
+        $book->year = $request->input('year');
+        $book->publisher = $request->input('publisher');
+        $book->body = $request->input('body');
+
+
+
+
+
+        $book->save();
+        return redirect()->route('book.index')
+            ->with('success', 'book created successfully.');
+    }
+    public function update(Request $request, Book $book)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:50',
+            'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'year' => 'required|integer',
+            'publisher' => 'required|string|max:50',
+            'body' => 'required|string',
+
+
+        ]);
+
+        if ($request->file('image') != '') {
+            $path = public_path("\storage\book\\") . $book->image;
+
+
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+
+
+            $name = $request->file('image')->getClientOriginalName();
+            $path = $request->file('image')->storeAs('public/book', $name);
+        } else {
+            $name = $book->image;
         }
-        Book::create($input);
+
+        $book->title = $request->input('title');
+        $book->image = $name;
+        $book->year = $request->input('year');
+        $book->publisher = $request->input('publisher');
+        $book->body = $request->input('body');
 
 
-        return back()
-            ->with('success', 'exhibitions made successfully.');
+
+
+
+        $book->update();
+        return redirect()->route('book.index')
+            ->with('success', 'album created successfully.');
     }
 
 
-    public function destroy(Book $book)
+    public function destroy($id)
     {
-        $book->delete();
-        return back()
-            ->with('success', 'book removed successfully.');
+        $book = Book::findOrFail($id);
+
+        $book->delete($id);
+        $path = public_path("\storage\book\\") . $book->image;
+        File::delete($path);
+
+
+        return redirect()->route('book.index')
+            ->with('success', 'Posts deleted successfully');
     }
 }
